a293905b8920f466726e82c9dd5d8328f7798b94
Pavlo Dyban <pavlo.dyban@doctolib.com>
2020-01-26 16:07:40 +0100

    Running version with BME280 & Wifi

--- esp8266/mqqt_client/mqqt_client.ino
+++ esp8266/mqqt_client/mqqt_client.ino
@@ -4,14 +4,29 @@
   License: MIT
 */
 
+// Select modules that you want to compile:
+//#define USING_SDS011
+#define USING_BME280
+//#define USING_DHT22
+
 // MQQT includes
 #include "EspMQTTClient.h"
 
 // SDS011 includes
-#include "SdsDustSensor.h"
+#ifdef USING_SDS011
+  #include "SdsDustSensor.h"
+#endif // USING_SDS011
 
 // DHT22 includes
-#include <DHT.h>
+#ifdef USING_DHT22
+  #include <DHT.h>
+#endif // USING_DHT22
+
+// BME280 includes
+#ifdef USING_BME280
+  #include <Adafruit_Sensor.h>
+  #include <Adafruit_BME280.h>
+#endif // USING_BME280
 
 // Wifi includes
 extern "C" {
@@ -33,14 +48,24 @@ EspMQTTClient client(
 );
 
 // SDS011 configuration
-int rxPin = D1; // rxPin has to be connected to TXD on SDS011 board
-int txPin = D2; // txPin has to be connected to RXD on SDS011 board
-SdsDustSensor sds(rxPin, txPin);
+#ifdef USING_SDS011
+  int rxPin = D1; // rxPin has to be connected to TXD on SDS011 board
+  int txPin = D2; // txPin has to be connected to RXD on SDS011 board
+  SdsDustSensor sds(rxPin, txPin);
+#endif // USING_SDS011
 
 // DHT22 configuration
-#define DHTPIN D7     // what digital pin the DHT22 is conected to
-#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
-DHT dht(DHTPIN, DHTTYPE);
+#ifdef USING_DHT22
+  #define DHTPIN D7     // what digital pin the DHT22 is conected to
+  #define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+  DHT dht(DHTPIN, DHTTYPE);
+#endif // USING_DHT22
+
+// BME280 configuration
+#ifdef USING_BME280
+  Adafruit_BME280 bme; 
+  #define SEALEVELPRESSURE_HPA (1013.25)
+#endif // USING_BME280
 
 // Wifi configuration
 #define FPM_SLEEP_MAX_TIME 0xFFFFFFF
@@ -72,11 +97,22 @@ void setup()
   Serial.println("setup()");
   Serial.begin(115200);
 
+  //setupLED();
+  //enableLED();
+
   setupMQTTClient();
-
+  
+#ifdef USING_SDS011
   setupSDS011();
+#endif // USING_SDS011
 
+#ifdef USING_DHT22
   setupDHT22();
+#endif // USING_DHT22
+  
+#ifdef USING_BME280
+  setupBME280();
+#endif // USING_BME280
 }
 
 void setupMQTTClient() {  
@@ -90,6 +126,26 @@ void setupMQTTClient() {
   client.enableLastWillMessage(debug_topic, "Client disconnected", true);  // You can activate the retain flag by setting the third parameter to true
 }
 
+
+//////////////////////////////////////////////////////
+// LED
+//////////////////////////////////////////////////////
+void setupLED() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+}
+
+void enableLED() {
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+}
+
+void disableLED() {
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+}
+
+//////////////////////////////////////////////////////
+// SDS 011
+//////////////////////////////////////////////////////
+#ifdef USING_SDS011
 void setupSDS011() {
   Serial.println("setupSDS011()");
   sds.begin();
@@ -99,47 +155,6 @@ void setupSDS011() {
   Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
   Serial.println(sds.setActiveReportingMode().toString()); // ensures sensor is in 'active' reporting mode
   Serial.println(sds.setContinuousWorkingPeriod().toString()); // ensures sensor has continuous working period - default but not recommended 
-}
-
-void setupDHT22() {
-  Serial.println("setupDHT22()");
-  dht.begin();
-}
-
-// This function is called once everything is connected (Wifi and MQTT)
-void onConnectionEstablished()
-{
-  client.publish(debug_topic, "Connected!"); // You can activate the retain flag by setting the third parameter to true
-  Serial.println("Connection with Wifi network established.");
-}
-
-void loop()
-{
-  client.loop();
-  Serial.println("loop()");
-
-  if(!client.isConnected()) { 
-    if(!client.isWifiConnected()) {
-      Serial.println("Could not establish Wifi connection.");
-    }
-    else if(!client.isMqttConnected()) {
-      Serial.println("Could not establish MQTT connection.");
-    }
-  }
-  else {
-    publishSDS011();
-  
-    publishDHT22();
-
-    delay(mqtt_publish_delay);
-  }
-
-  disableSDS011();
-
-  disableWifi();
-  
-  Serial.println("Going into deep sleep");
-  ESP.deepSleep(refresh_delay*1e3, WAKE_RF_DEFAULT);
 }
 
 void publishSDS011() {
@@ -164,6 +179,16 @@ void disableSDS011() {
   else {
     Serial.println("Successfully put sensor to sleep");
   }
+}
+#endif // USING_SDS011
+
+//////////////////////////////////////////////////////
+// DHT 22
+//////////////////////////////////////////////////////
+#ifdef USING_DHT22
+void setupDHT22() {
+  Serial.println("setupDHT22()");
+  dht.begin();
 }
 
 void publishDHT22() {
@@ -194,4 +219,97 @@ void publishDHT22() {
   Serial.print("Heat index: ");
   Serial.print(hic);
   Serial.println(" *C ");
+}
+#endif // USING_DHT22
+
+//////////////////////////////////////////////////////
+// BME 280
+//////////////////////////////////////////////////////
+#ifdef USING_BME280
+void setupBME280() {
+  Serial.println("setupBME280()");
+  if(!bme.begin(0x76)) {
+    Serial.println("Could not establish connection to BME280");
+  }
+  else {
+    Serial.println("Successfully established connection to BME280");
+  }
+}
+
+void publishBME280() {
+  Serial.println("publishBME280()");
+  
+  float t, h, p, a;
+  t = bme.readTemperature();
+  h = bme.readHumidity();
+  p = bme.readPressure() / 100.0F;
+  a = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" % ");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print("Pressure: ");
+  Serial.print(p);
+  Serial.print(" Pa ");
+  Serial.print("Altitude: ");
+  Serial.print(a);
+  Serial.println(" m");
+
+  client.publish(temperature_topic, String(t).c_str());
+  client.publish(humidity_topic, String(h).c_str());   
+  client.publish(pressure_topic, String(p).c_str());    
+}
+#endif // USING_BME280
+
+// This function is called once everything is connected (Wifi and MQTT)
+void onConnectionEstablished()
+{
+  client.publish(debug_topic, "Connected!"); // You can activate the retain flag by setting the third parameter to true
+  Serial.println("Connection with Wifi network established.");
+}
+
+void loop()
+{
+  client.loop();
+  Serial.println("loop()");
+
+  if(!client.isConnected()) { 
+    if(!client.isWifiConnected()) {
+      Serial.println("Could not establish Wifi connection.");
+    }
+    else if(!client.isMqttConnected()) {
+      Serial.println("Could not establish MQTT connection.");
+    }
+  }
+  else {
+#ifdef USING_SDS011
+    publishSDS011();
+#endif // USING_SDS011
+
+#ifdef USING_DHT22
+    publishDHT22();
+#endif // USING_DHT22
+  
+#ifdef USING_BME280
+    publishBME280();
+#endif // USING_BME280
+
+    delay(mqtt_publish_delay);
+  }
+
+#ifdef USING_SDS011
+  disableSDS011();
+#endif // USING_SDS011
+
+  disableWifi();
+
+  //disableLED();
+  
+  Serial.print("Going into deep sleep for ");
+  Serial.print(refresh_delay/1000);
+  Serial.println("s");
+  ESP.deepSleep(refresh_delay*1e3, WAKE_RF_DEFAULT);
 }
--- esp8266/mqqt_client/smart_home_config.h
+++ esp8266/mqqt_client/smart_home_config.h
@@ -1,23 +1,24 @@
 // WIFI settings
-#define wifi_ssid "my_wifi_ssid"
+#define wifi_ssid     "my_wifi_ssid"
 #define wifi_password "my_wifi_password"
 
 // MQQT settings
-#define mqtt_server "my_mqtt_broker_ip"
-#define mqtt_user "my_mqtt_broker_username"
-#define mqtt_password "my_mqtt_broker_password"
+#define mqtt_server      "my_mqtt_broker_ip"
+#define mqtt_user        "my_mqtt_broker_username"
+#define mqtt_password    "my_mqtt_broker_password"
 #define mqtt_client_name "my_mqtt_broker_name"
 #define mqtt_server_port my_mqtt_broker_port
 
-#define humidity_topic "sensor/humidity"
+#define humidity_topic    "sensor/humidity"
 #define temperature_topic "sensor/temperature"
-#define pm10_topic "sensor/pm10"
-#define pm25_topic "sensor/pm25"
-#define debug_topic "sensor/debug"
+#define pressure_topic    "sensor/pressure"
+#define pm10_topic        "sensor/pm10"
+#define pm25_topic        "sensor/pm25"
+#define debug_topic       "sensor/debug"
 
 // delay between alive cycles in ms
-#define refresh_delay 5000
+#define refresh_delay      30000 // previous value: 10000
 // delay after wifi (dis-)connect functions in ms
-#define wifi_delay 1000
+#define wifi_delay         2000  // best value: 3000
 // delay after publishing MQTT messages in ms
-#define mqtt_publish_delay 50
+#define mqtt_publish_delay 150
